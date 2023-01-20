9506ab501c1a97a21569c00554152a64a2cd41d6
Jcorvil <jcorvil600@g.educaand.es>
2023-01-20 13:54:05 +0100

    Ejercicio recorrerArray

--- src/RecorrerArrayImprime/Main.java
+++ src/RecorrerArrayImprime/Main.java
@@ -4,14 +4,19 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] a = {2,4,6,8,10};
+        int [] a = {2,4,6,8,10,12};
         metodo(a);
-        System.out.println(Arrays.toString(a));
+        //System.out.println(Arrays.toString(a));
     }
 
     static void metodo(int[] a){
+        System.out.print("{");
         for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+            System.out.print(a[i]);
+            if (i < a.length - 1){
+                System.out.print(", ");
+            }
         }
+        System.out.print("}");
     }
 }
